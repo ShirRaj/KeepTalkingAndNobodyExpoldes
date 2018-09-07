621f983d00f0054e03dab0b6dfd43e2a5f2c806b
Tal Taub <taltaub22@gmail.com>
2018-09-07 03:10:51 +0300

    Basic UI

--- rpi/webserver/main.py
+++ rpi/webserver/main.py
@@ -1,11 +1,31 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, redirect
 
 app = Flask(__name__)
 
 with app.test_request_context():
     url_for('static', filename="*")
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', wires=['red', None, 'red', 'green', None], ready=True, started=True)
 
+
+@app.route('/start')
+def start_time_route():
+    start_time()
+    return redirect('/')
+
+
+@app.route('/reset')
+def reset_game_route():
+    reset_game()
+    return redirect('/')
+
+
+def start_time():
+    print "Starting time"
+
+
+def reset_game():
+    print "Resetting game"
