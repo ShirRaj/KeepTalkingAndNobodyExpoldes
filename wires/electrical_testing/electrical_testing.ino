ee7c800db773f3220394cb5b6949fc257c5f4086
yotam.kenneth@gmail.com <yotam.kenneth@gmail.com>
2018-09-08 11:26:56 +0300

    Fix PIN numbers for SEL
    Minor logic fixes

--- wires/electrical_testing/electrical_testing.ino
+++ wires/electrical_testing/electrical_testing.ino
@@ -2,8 +2,8 @@
 #define BASE_MINUS_PIN 0
 #define BASE_SCREW_PIN 5
 
-#define SEL_BASE_PIN 3
-#define READ_PIN 7
+#define SEL_BASE_PIN 6
+#define READ_PIN 2
 
 #define NUM_SWITCHES 5
 
@@ -11,18 +11,16 @@
 void setup() {
   Serial.begin(9600);
   for(int  i = 0; i < MULTIPLEXER_PINS; i++){
-    pinMode(SEL_BASE_PIN + i, OUTPUT);
+    pinMode(SEL_BASE_PIN - i, OUTPUT);
   }
   pinMode(READ_PIN, INPUT);
 }
 
 void loop() {
   int val = 99;
-  for(int i = 15; i > 15-NUM_SWITCHES*2; i--) {
-    if (i!= 14 && i!=10&&i!=13) {
-      val = read_pin(i);
-      Serial.println(" PIN " + String(i) +" VAL IS " + String(val));
-    }
+  for(int i = 15; i > 5; i--) {
+    val = read_pin(i);
+    Serial.println(" PIN " + String(i) +" VAL IS " + String(val));
   }
   delay(1000);
   
@@ -31,7 +29,8 @@ void loop() {
 int read_pin(int pin) {
   //LSB to MSB
   for(int i = 0; i < MULTIPLEXER_PINS; i++) {
-    digitalWrite(SEL_BASE_PIN+i, pin % 2);
+    //Serial.println(String(SEL_BASE_PIN-i) + " - " + String(pin % 2));
+    digitalWrite(SEL_BASE_PIN-i, pin % 2);
     pin = pin / 2;
   }
   return digitalRead(READ_PIN);
--- wires/wires.ino
+++ wires/wires.ino
@@ -33,8 +33,8 @@ int prev_state;
 #define BASE_MINUS_PIN 7
 #define BASE_SCREW_PIN A0
 
-#define SEL_BASE_PIN 1
-#define READ_PIN 1
+#define SEL_BASE_PIN 6
+#define READ_PIN 2
 
 #define STATE_TNT_THREE_SAME 0
 #define STATE_TNT_THREE_OR_FOUR_TWO_WHITE 1
@@ -56,6 +56,7 @@ int prev_state;
 
 
 #define MULTIPLEXER_BITS 4
+
 String MODULE_ID =  "wires";
 
 String raw_msg;
@@ -67,10 +68,11 @@ int cur_time = 0;
 int color_row [NUM_SWITCHES];
 int state;
 
-
-int correct_plus_switches [] = {-1,-1,-1,-1,-1};
-int correct_minus_switches [] = {-1,-1,-1,-1,-1};
-int correct_wires []  = {-1,-1,-1,-1,-1};
+int cur_trigger = 0;
+int cur_penalty = 0;
+int correct_plus_switches [] = { -1, -1, -1, -1, -1};
+int correct_minus_switches [] = { -1, -1, -1, -1, -1};
+int correct_wires []  = { -1, -1, -1, -1, -1};
 
 int right_wires;
 int wrong_wires;
@@ -83,12 +85,12 @@ void setup()
 {
   prev_state = -1;
   cur_state = STATE_BOOT;
-  for(int i = 0; i < NUM_SWITCHES; i ++) {
-    pinMode(BASE_SCREW_PIN+i, INPUT_PULLUP);
+  for (int i = 0; i < NUM_SWITCHES; i ++) {
+    pinMode(BASE_SCREW_PIN + i, INPUT_PULLUP);
   }
 
-  for(int i=0;i < MULTIPLEXER_BITS;i++) {
-    pinMode(SEL_BASE_PIN+i, OUTPUT);
+  for (int i = 0; i < MULTIPLEXER_BITS; i++) {
+    pinMode(SEL_BASE_PIN - i, OUTPUT);
   }
 
   pinMode(READ_PIN, INPUT);
@@ -101,100 +103,100 @@ void loop()
   int enter_state = 0;
   if (cur_state != prev_state) {
     enter_state = 1;
-    prev_state=cur_state;
+    prev_state = cur_state;
   }
   raw_msg = "";
   msg = "";
   data = "";
-    // if we get a valid byte, read analog ins:
+  // if we get a valid byte, read analog ins:
   if (Serial.available() > 0) {
-      raw_msg = Serial.readString();
-      int index_of_space = raw_msg.indexOf(" ");
-      int str_length = raw_msg.length();
-      
-      if (index_of_space > 0) {
-        msg = raw_msg.substring(0,index_of_space);
-        data =raw_msg.substring(index_of_space+1,str_length-1);
-      }
-      else 
-        msg = raw_msg.substring(0,str_length-1);
-      
-      
-      if(msg=="TIME") {
-       cur_time = data.toInt();
-      }
-      else if(msg=="DATA"){
-        process_data(data);
-      }
-      else if(msg=="PING"){
-        send_msg(MSG_PING,"");
-      }
+    raw_msg = Serial.readString();
+    int index_of_space = raw_msg.indexOf(" ");
+    int str_length = raw_msg.length();
+
+    if (index_of_space > 0) {
+      msg = raw_msg.substring(0, index_of_space);
+      data = raw_msg.substring(index_of_space + 1, str_length - 1);
+    }
+    else
+      msg = raw_msg.substring(0, str_length - 1);
+
+
+    if (msg == "TIME") {
+      cur_time = data.toInt();
+    }
+    else if (msg == "DATA") {
+      process_data(data);
+    }
+    else if (msg == "PING") {
+      send_msg(MSG_PING, "");
+    }
   }
-  
-  switch(cur_state) {
-    
+
+  switch (cur_state) {
+
     case STATE_BOOT:
-      if(enter_state)
+      if (enter_state)
         boot_on_enter();
       break;
 
     case STATE_INIT:
-      if(enter_state)
+      if (enter_state)
         init_on_enter();
-      if(check_if_ready()) {
-        cur_state=STATE_READY;
+      if (check_if_ready()) {
+        cur_state = STATE_READY;
       }
       break;
 
     case STATE_READY:
-      if(enter_state)
+      if (enter_state)
         ready_on_enter();
-        
-      if(!check_if_ready()) {
-        cur_state=STATE_INIT;
-        send_msg(MSG_UNREADY,"");
+
+      if (!check_if_ready()) {
+        cur_state = STATE_INIT;
+        send_msg(MSG_UNREADY, "");
       }
-      
-      if(msg=="START"){
+
+      if (msg == "START") {
         cur_state = STATE_START;
-        send_msg(MSG_ACK,"");
+        send_msg(MSG_ACK, "");
       }
       break;
 
     case STATE_START:
-      if(enter_state)
+      if (enter_state)
         start_on_enter();
 
-      if(msg=="BOOM"){
+      if (msg == "BOOM") {
         cur_state = STATE_BOOM;
         break;
       }
-      
+
       process_logic();
-      
-      if(check_trigger()){
-        send_msg(MSG_TRIGGER,"");
+
+      if (check_trigger()) {
+        send_msg(MSG_TRIGGER, "");
       }
 
-      if(check_penalty()) {
-        send_msg(MSG_PENALTY,"");
+      if (check_penalty()) {
+        send_msg(MSG_PENALTY, "");
       }
 
-      if(check_solved()) { 
+      if (check_solved()) {
         cur_state = STATE_SOLVED;
       }
       break;
 
     case STATE_BOOM:
-      if(enter_state)
+      if (enter_state)
         boom_on_enter();
       break;
 
     case STATE_SOLVED:
-      if(enter_state)
+      if (enter_state)
         solved_on_enter();
       break;
-      
+
     default:
       break;
   }
@@ -205,50 +207,63 @@ void loop()
 //EDIT FROM HERE
 
 void process_logic() {
-  int right_wires = 1;
-  int wrong_wires = 0;
-  int right_switches = 1;
-  int wrong_switches = 0;
+  right_wires = 1;
+  wrong_wires = 0;
+  right_switches = 1;
+  wrong_switches = 0;
+  int wire_states [NUM_SWITCHES];
+  for (int i = 0; i < NUM_SWITCHES; i++) {
+    wire_states[i] = (digitalRead(BASE_SCREW_PIN + i) == LOW);
+  }
 
-  for (int i =0; i < NUM_SWITCHES; i++) {
-    if(correct_wires[i] >=0 && digitalRead(BASE_SCREW_PIN+i)==HIGH){
-      right_wires = 0;
+  for (int i = 0; i < NUM_SWITCHES; i++) {
+    if (correct_wires[i] >= 0 ) {
+
+      if (wire_states[correct_wires[i]] == 1) {
+        Serial.println("WIRE " + String(correct_wires[i]) + " IS STILL CONNECTED");
+        right_wires = 0;
+      }
+      wire_states[correct_wires[i]] = 0;
     }
   }
-  for(int i = 0; i < NUM_SWITCHES; i++) {
-    if(color_row[i] != EMPTY) {
-      if(digitalRead(BASE_SCREW_PIN+i)==LOW) {
-        wrong_wires = 1;
-      }
+  for (int i = 0; i < NUM_SWITCHES; i++) {
+    if (color_row[i] != EMPTY && wire_states[i] == 0) {
+      Serial.println("WIRE " + String(i) + " IS WRONGLY DISCONNECTED");
+      wrong_wires = 1;
     }
   }
   int plus_switch_state [NUM_SWITCHES];
   int minus_switch_state [NUM_SWITCHES];
-  for(int i = 0; i < NUM_SWITCHES; i++) {
-    plus_switch_state[i] = (read_pin(BASE_PLUS_PIN+2*i)==LOW);
-    minus_switch_state[i] = (read_pin(BASE_MINUS_PIN+2*i)==LOW);
+  for (int i = 0; i < NUM_SWITCHES; i++) {
+    plus_switch_state[i] = (read_pin(BASE_PLUS_PIN + 2 * i) == LOW);
+    minus_switch_state[i] = (read_pin(BASE_MINUS_PIN + 2 * i) == LOW);
+    //    Serial.println("PIN " + String(i) + " PLUS " + String(plus_switch_state[i]) + " MINUS " + String(minus_switch_state[i]));
   }
 
   for (int i = 0; i < NUM_SWITCHES; i++) {
-    if(correct_plus_switches[i] >= 0) {
-      if(plus_switch_state[correct_plus_switches[i]] == 0)
-        right_switches=0;
-      else
-        plus_switch_state[correct_plus_switches[i]] = 0;
+    if (correct_plus_switches[i] >= 0) {
+      if (plus_switch_state[correct_plus_switches[i]] == 1) {
+        right_switches = 0;
+        Serial.println("PLUS SWITCH " + String(correct_plus_switches[i]) + " IS NOT SWITCHED");
+      }
+      plus_switch_state[correct_plus_switches[i]] = 1;
     }
-    if(correct_minus_switches[i] >= 0) {
-      if(minus_switch_state[correct_minus_switches[i]] == 0)
-        right_switches=0;
-      else
-        minus_switch_state[correct_minus_switches[i]] = 0;    
+    if (correct_minus_switches[i] >= 0) {
+      if (minus_switch_state[correct_minus_switches[i]] == 1) {
+        Serial.println("MINUS SWITCH " + String(correct_minus_switches[i]) + " IS NOT SWITCHED");
+        right_switches = 0;
+      }
+      minus_switch_state[correct_minus_switches[i]] = 1;
     }
   }
 
   for (int i = 0; i < NUM_SWITCHES; i++) {
-    if(plus_switch_state[correct_plus_switches[i]]) {
+    if (plus_switch_state[i] == 0) {
+      Serial.println("PLUS SWITCH " + String(i) + " IS WRONGLY SWITCHED");
       wrong_switches = 1;
     }
-    if(plus_switch_state[correct_minus_switches[i]]) {
+    if (minus_switch_state[i] == 0) {
+      Serial.println("MINUS SWITCH " + String(i) + " IS WRONGLY SWITCHED");
       wrong_switches = 1;
     }
   }
@@ -257,32 +272,45 @@ void process_logic() {
 
 boolean check_trigger() {
   //check if user triggered bomb
-  if(wrong_switches || wrong_wires) {
-    return true;
+  if ((wrong_switches || wrong_wires)) {
+    if (cur_trigger == 0) {
+      return true;
+    }
+    cur_trigger = 1;
   }
+  else
+    cur_trigger = 0;
   return false;
 }
 
 boolean check_penalty() {
   //chek if user needs a penalty
-  if (correct_wires==1 && correct_switches==0){
-    return true; 
+  
+  if (right_wires == 1 && right_switches == 0) {
+    if(cur_penalty == 0) {
+      return true;
+    }
+    cur_penalty = 1;
+
   }
+  else
+    cur_penalty = 0;
   return false;
 }
 
 boolean check_solved() {
-  if (correct_wires==1 && correct_switches==1){
+  Serial.println("right wires " + String(right_wires) + " right switches " + String(right_switches));
+  if (right_wires == 1 && right_switches == 1) {
     return true;
   }
   return false;
 }
 
 void send_msg(String msg, String data) {
-  if(data!=NULL){
-    Serial.println(msg+" "+data);
+  if (data != NULL) {
+    Serial.println(msg + " " + data);
   }
-  else{
+  else {
     Serial.println(msg);
   }
 }
@@ -293,19 +321,19 @@ void process_data(String data) {
   int str_length = data.length();
   String in_data;
   String msg;
-  
+
   if (index_of_delim > 0) {
-    msg = data.substring(0,index_of_delim);
-    in_data =data.substring(index_of_delim+1,str_length);
+    msg = data.substring(0, index_of_delim);
+    in_data = data.substring(index_of_delim + 1, str_length);
   }
   else {
     Serial.println("ERROR: no delimiter found");
     return;
   }
 
-  if(msg==COLOR_ROW_DATA) {
-    for(int i = 0; i < NUM_SWITCHES; i ++) {
-      switch(in_data.charAt(2*i)){
+  if (msg == COLOR_ROW_DATA) {
+    for (int i = 0; i < NUM_SWITCHES; i ++) {
+      switch (in_data.charAt(2 * i)) {
         case 'Y':
           color_row[i] = YELLOW;
           break;
@@ -323,12 +351,13 @@ void process_data(String data) {
           break;
         case 'E':
           color_row[i] = EMPTY;
-          break;    
+          break;
       }
     }
-  }  
-  else if(msg==STATE_DATA) {
-    state = in_data.toInt(); 
+  }
+  else if (msg == STATE_DATA) {
+    Serial.println("Feeding STATE DATA");
+    state = in_data.toInt();
     int left;
     int right;
     int middle;
@@ -338,14 +367,14 @@ void process_data(String data) {
 
     switch (state) {
       case STATE_TNT_THREE_SAME:
-        left = NUM_SWITCHES+1;
+        left = NUM_SWITCHES + 1;
         right = -1;
         middle = -1;
-        for(int i = 0; i < NUM_SWITCHES; i++){
-          if (color_row[i]!=EMPTY) {
-            if(left > i)
+        for (int i = 0; i < NUM_SWITCHES; i++) {
+          if (color_row[i] != EMPTY) {
+            if (left > i)
               left = i;
-            else if(right < 0)
+            else if (right < 0)
               right = i;
             else {
               middle = right;
@@ -358,40 +387,40 @@ void process_data(String data) {
         correct_wires[0] = middle;
         break;
       case STATE_TNT_THREE_OR_FOUR_TWO_WHITE:
-        left = NUM_SWITCHES+1;
+        left = NUM_SWITCHES + 1;
         right = -1;
         middle = -1;
-        for(int i = 0; i < NUM_SWITCHES; i++){
-          if (color_row[i]==WHITE) {
-            if(left > i)
+        for (int i = 0; i < NUM_SWITCHES; i++) {
+          if (color_row[i] == WHITE) {
+            if (left > i)
               left = i;
-            else if(right < i)
+            else if (right < i)
               right = i;
           }
         }
         correct_plus_switches[0] = right;
         correct_minus_switches[0] = left;
         counter = 0;
-        for(int i = 0; i < NUM_SWITCHES; i++) {
-          if(color_row[i] != EMPTY) {
+        for (int i = 0; i < NUM_SWITCHES; i++) {
+          if (color_row[i] != EMPTY) {
             correct_wires[counter] = i;
             counter++;
           }
         }
         break;
-       
+
       case STATE_TNT_THREE_OR_FOUR_TWO_YELLOW:
-        left = NUM_SWITCHES+1;
+        left = NUM_SWITCHES + 1;
         right = -1;
         middle = -1;
-        for(int i = 0; i < NUM_SWITCHES; i++){
-          if (color_row[i]==YELLOW) {
-            if(left > i)
+        for (int i = 0; i < NUM_SWITCHES; i++) {
+          if (color_row[i] == YELLOW) {
+            if (left > i)
               left = i;
-            else if(right < i)
+            else if (right < i)
               right = i;
           }
-          if(color_row[i] != EMPTY && left < NUM_SWITCHES && right < 0) {
+          if (color_row[i] != EMPTY && left < NUM_SWITCHES && right < 0) {
             middle = i;
           }
         }
@@ -406,7 +435,7 @@ void process_data(String data) {
       case STATE_TNT_FIVE_THREE_SAME:
         correct_plus_switches [0] = 4;
         correct_minus_switches[0] = 0;
-        for(int i = 0; i < NUM_SWITCHES; i++) {
+        for (int i = 0; i < NUM_SWITCHES; i++) {
           correct_wires[i] = i;
         }
         break;
@@ -414,8 +443,8 @@ void process_data(String data) {
       case STATE_TNT_FIVE_TWO_RED:
         red_counter = 0;
         counter = 0;
-        for(int i = 0; i < NUM_SWITCHES; i ++) {
-          if(color_row[i] == RED) {
+        for (int i = 0; i < NUM_SWITCHES; i ++) {
+          if (color_row[i] == RED) {
             correct_plus_switches[red_counter] = i;
             red_counter = red_counter + 1;
           }
@@ -430,9 +459,9 @@ void process_data(String data) {
       case STATE_TNT_FIVE_TWO_GREEN:
         green_counter = 0;
         counter = 0;
-        for(int i = 0; i < NUM_SWITCHES; i ++) {
-          
-          if(color_row[i] == GREEN) {
+        for (int i = 0; i < NUM_SWITCHES; i ++) {
+
+          if (color_row[i] == GREEN) {
             correct_plus_switches[green_counter] = i;
             red_counter = green_counter + 1;
           }
@@ -446,14 +475,14 @@ void process_data(String data) {
 
 
       case STATE_DYNAMITE_THREE_SAME:
-        left = NUM_SWITCHES+1;
+        left = NUM_SWITCHES + 1;
         right = -1;
         middle = -1;
-        for(int i = 0; i < NUM_SWITCHES; i++){
-          if (color_row[i]!=EMPTY) {
-            if(left > i)
+        for (int i = 0; i < NUM_SWITCHES; i++) {
+          if (color_row[i] != EMPTY) {
+            if (left > i)
               left = i;
-            else if(right < 0)
+            else if (right < 0)
               right = i;
             else {
               middle = right;
@@ -466,40 +495,40 @@ void process_data(String data) {
         correct_wires[0] = middle;
         break;
       case STATE_DYNAMITE_THREE_OR_FOUR_TWO_WHITE:
-        left = NUM_SWITCHES+1;
+        left = NUM_SWITCHES + 1;
         right = -1;
         middle = -1;
-        for(int i = 0; i < NUM_SWITCHES; i++){
-          if (color_row[i]==WHITE) {
-            if(left > i)
+        for (int i = 0; i < NUM_SWITCHES; i++) {
+          if (color_row[i] == WHITE) {
+            if (left > i)
               left = i;
-            else if(right < i)
+            else if (right < i)
               right = i;
           }
         }
         correct_plus_switches[0] = left;
         correct_minus_switches[0] = right;
         counter = 0;
-        for(int i = 0; i < NUM_SWITCHES; i++) {
-          if(color_row[i] != EMPTY) {
+        for (int i = 0; i < NUM_SWITCHES; i++) {
+          if (color_row[i] != EMPTY) {
             correct_wires[counter] = i;
             counter++;
           }
         }
         break;
-       
+
       case STATE_DYNAMITE_THREE_OR_FOUR_TWO_YELLOW:
-        left = NUM_SWITCHES+1;
+        left = NUM_SWITCHES + 1;
         right = -1;
         middle = -1;
-        for(int i = 0; i < NUM_SWITCHES; i++){
-          if (color_row[i]==YELLOW) {
-            if(left > i)
+        for (int i = 0; i < NUM_SWITCHES; i++) {
+          if (color_row[i] == YELLOW) {
+            if (left > i)
               left = i;
-            else if(right < i)
+            else if (right < i)
               right = i;
           }
-          if(color_row[i] != EMPTY && left < NUM_SWITCHES && right < 0) {
+          if (color_row[i] != EMPTY && left < NUM_SWITCHES && right < 0) {
             middle = i;
           }
         }
@@ -514,7 +543,7 @@ void process_data(String data) {
       case STATE_DYNAMITE_FIVE_THREE_SAME:
         correct_minus_switches[0] = 4;
         correct_plus_switches[0] = 0;
-        for(int i = 0; i < NUM_SWITCHES; i++) {
+        for (int i = 0; i < NUM_SWITCHES; i++) {
           correct_wires[i] = i;
         }
         break;
@@ -522,8 +551,8 @@ void process_data(String data) {
       case STATE_DYNAMITE_FIVE_TWO_RED:
         red_counter = 0;
         counter = 0;
-        for(int i = 0; i < NUM_SWITCHES; i ++) {
-          if(color_row[i] == RED) {
+        for (int i = 0; i < NUM_SWITCHES; i ++) {
+          if (color_row[i] == RED) {
             correct_plus_switches[red_counter] = i;
             red_counter = red_counter + 1;
           }
@@ -538,9 +567,9 @@ void process_data(String data) {
       case STATE_DYNAMITE_FIVE_TWO_GREEN:
         green_counter = 0;
         counter = 0;
-        for(int i = 0; i < NUM_SWITCHES; i ++) {
-          
-          if(color_row[i] == GREEN) {
+        for (int i = 0; i < NUM_SWITCHES; i ++) {
+
+          if (color_row[i] == GREEN) {
             correct_minus_switches[green_counter] = i;
             red_counter = green_counter + 1;
           }
@@ -551,9 +580,9 @@ void process_data(String data) {
         break;
     }
 
-    for(int i = 0; i < NUM_SWITCHES; i++) {
-      if(correct_wires[i] >= 0) {
-        color_row[correct_wires[i]] = EMPTY;
+    for (int i = 0; i < NUM_SWITCHES; i++) {
+      if (correct_wires[i] >= 0) {
+        //color_row[correct_wires[i]] = EMPTY;
       }
     }
   }
@@ -563,25 +592,39 @@ void process_data(String data) {
 }
 
 boolean check_if_ready() {
-  for(int i = 0; i < NUM_SWITCHES; i++) {
-    if(color_row[i] != EMPTY) {
-      if(digitalRead(BASE_SCREW_PIN+i)==HIGH) {
+  if (correct_plus_switches[0] == -1 && correct_minus_switches[0] == -1 && correct_wires[0] == -1) {
+    Serial.println("No data fed");
+    return false;
+  }
+
+  for (int i = 0; i < NUM_SWITCHES; i++) {
+    if (color_row[i] != EMPTY) {
+      if (digitalRead(BASE_SCREW_PIN + i) == HIGH) {
+        Serial.println("SCREW " + String(i) + " not ready");
+        return false;
+      }
+    }
+    else {
+      if (digitalRead(BASE_SCREW_PIN + i) == LOW) {
+        Serial.println("SCREW " + String(i) + " not ready");
         return false;
       }
     }
   }
-  for(int i = 0; i < NUM_SWITCHES; i++) {
-    if(read_pin(BASE_MINUS_PIN+2*i)==LOW || read_pin(BASE_PLUS_PIN+2*i)==LOW) {
+  for (int i = 0; i < NUM_SWITCHES; i++) {
+    if (read_pin(BASE_MINUS_PIN + 2 * i) == HIGH || read_pin(BASE_PLUS_PIN + 2 * i) == HIGH) {
+      Serial.println("PLUS or MINUS SWITCH " + String(i) + " not ready");
       return false;
     }
   }
+
   return true;
 }
 
 int read_pin(int pin) {
   //LSB to MSB
-  for(int i = 0; i < MULTIPLEXER_BITS; i++) {
-    digitalWrite(SEL_BASE_PIN+i, pin % 2);
+  for (int i = 0; i < MULTIPLEXER_BITS; i++) {
+    digitalWrite(SEL_BASE_PIN - i, pin % 2);
     pin = pin / 2;
   }
   return digitalRead(READ_PIN);
@@ -590,7 +633,7 @@ int read_pin(int pin) {
 //FSM functions
 
 void boot_on_enter() {
-  send_msg(MSG_BOOT,MODULE_ID);
+  send_msg(MSG_BOOT, MODULE_ID);
   cur_state = STATE_INIT;
 }
 
@@ -598,7 +641,7 @@ void init_on_enter() {
 }
 
 void ready_on_enter () {
-  send_msg(MSG_READY,"");
+  send_msg(MSG_READY, "");
 }
 
 void start_on_enter() {
@@ -608,5 +651,5 @@ void boom_on_enter() {
 }
 
 void solved_on_enter () {
-  send_msg(MSG_SOLVED,"");
+  send_msg(MSG_SOLVED, "");
 }
